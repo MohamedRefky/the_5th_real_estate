c948c82d12b1bead7614c0de62670e0ee7cb075a
Mohamed Refky <mohamedrifky9765@gmail.com>
2026-08-07 19:41:50 +0300

    feat: initialize application with GoogleFonts configuration and entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  GoogleFonts.config.allowRuntimeFetching = true;
   runApp(const TheApp());
 }
