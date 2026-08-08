54c0f3751304a1bfeeb67f3f60ac1fae0b86743f
Mohamed Refky <mohamedrifky9765@gmail.com>
2026-08-08 17:18:02 +0300

    feat: add FeaturedPropertiesSection widget and centralize theme configuration with AppTheme

--- lib/screens/home/widgets/featured_properties_section.dart
+++ lib/screens/home/widgets/featured_properties_section.dart
@@ -28,7 +28,7 @@ class FeaturedPropertiesSection extends StatelessWidget {
               ),
               const SizedBox(height: 20),
               SizedBox(
-                height: 385,
+                height: 410,
                 child: ListView.separated(
                   padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
                   scrollDirection: Axis.horizontal,
