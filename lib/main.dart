06a44be4c7c885f7e4b7276b8e819d4af003c339
Mohamed Refky <mohamedrifky9765@gmail.com>
2026-08-09 21:19:22 +0300

    feat: initialize Firebase with multi-platform configuration and path URL strategy

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:the_5th_real_estate/firebase_options.dart';
+import 'package:the_5th_real_estate/core/firebase/firebase_options.dart';
 
 import 'app/app.dart';
 
