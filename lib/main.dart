3f6ce1ebe19687ad65c151142a0e1dc104d0e8e8
Mohamed Refky <mohamedrifky9765@gmail.com>
2026-08-09 17:54:09 +0300

    feat: integrate Firebase and add go_router dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:the_5th_real_estate/firebase_options.dart';
 
 import 'app/app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   GoogleFonts.config.allowRuntimeFetching = true;
 
   runApp(const TheApp());
