fe77775c0f7c9460e874def143beee95cbf2ccdc
Mohamed Refky <mohamedrifky9765@gmail.com>
2026-09-12 21:41:00 +0300

    feat: add web platform files including Cloudflare headers, bootstrap script, and custom index.html

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -2,6 +2,9 @@
 {{flutter_build_config}}
 
 _flutter.loader.load({
+  config: {
+    canvasKitBaseUrl: "canvaskit/"
+  },
   onEntrypointLoaded: async function(engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
     await appRunner.runApp();
